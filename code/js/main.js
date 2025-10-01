98393c25b1a7fbc9b28ab4875a4f0e15362d592c
Myz09 <160198749+Myz09@users.noreply.github.com>
2025-10-01 17:38:33 -0400

    Prepping js

---
+++ code/js/main.js
@@ -0,0 +1,11 @@
+var currentPage="Home";
+var title="cherielle";
+if (currentPage=="Home"){
+    pass;
+}else if(currentPage=="Mission"){
+    pass;
+}else if(currentPage=="Articles"){
+    pass;
+}else if(currentPage=="Contact"){
+    pass;
+}
