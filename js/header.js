33b92e9db605bae253475598ac781c38f3843122
Omya Tomar <157304001+OmyaT@users.noreply.github.com>
2026-05-31 23:10:31 -0500

    add tutoring link to header navigation

--- js/header.js
+++ js/header.js
@@ -9,7 +9,9 @@ header.innerHTML = `
 <li><a href = "about.html">About</a></li>
 <li><a href = "events.html">Events</a></li>
 <li><a href = "blog.html">Blog</a></li>
+<li><a href= "tutoring.html">Tutoring</a></li>
 <li><a href= "contact.html">Contact</a></li>
+
 </ul> 
 </nav>  
 <a href="https://linktr.ee/cherielle" id="linktree">Linktree</a>
